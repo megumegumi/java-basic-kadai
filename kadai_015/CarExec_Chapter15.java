67032b5ad5a944efc46a4128b703bb52edcd4559
megumegumi <a.flourrose1223@icloud.com>
2025-04-08 01:56:47 +0900

    Add files via upload

---
+++ kadai_015/CarExec_Chapter15.java
@@ -0,0 +1,13 @@
+package kadai_015;
+
+public class CarExec_Chapter15 {
+
+	public static void main(String[] args) {
+		Car_Chapter15 speed = new Car_Chapter15(0, 0);
+				speed.gearChange(3);
+				speed.run();
+				
+			}
+	}
+
+
