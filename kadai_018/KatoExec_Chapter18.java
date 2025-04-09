898fdb63cdb59a29363e42887559b11636affda8
megumegumi <a.flourrose1223@icloud.com>
2025-04-09 22:47:42 +0900

    Add files via upload

--- kadai_018/KatoExec_Chapter18.java
+++ kadai_018/KatoExec_Chapter18.java
@@ -2,9 +2,9 @@ package kadai_018;
 
 public class KatoExec_Chapter18 {
     public static void main(String[] args) {
-        KatoTaro_Chapter18 taro = new KatoTaro_Chapter18();
-        KatoIchiro_Chapter18 ichiro = new KatoIchiro_Chapter18();
-        KatoHanako_Chapter18 hanako = new KatoHanako_Chapter18();
+        KatoTaro_Chapter18 taro = new KatoTaro_Chapter18();//taro.execIntroduce();表示される
+        KatoIchiro_Chapter18 ichiro = new KatoIchiro_Chapter18();//ichiro.execIntroduce();表示される
+        KatoHanako_Chapter18 hanako = new KatoHanako_Chapter18();//hanako.execIntroduce();表示される
 
         taro.execIntroduce();
         ichiro.execIntroduce();
